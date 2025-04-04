0c814c95c38a914e11f5853741a7923b393bbe31
ywallis <yann.wallis@gmail.com>
2025-04-04 08:29:03 +0700

    moved REPL to separate file

--- main.go
+++ main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
-func cleanInput(text string) []string {
-	lower := strings.ToLower(text)
-	split := strings.Fields(lower)
-
-	return split 
-}
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := cleanInput(scanner.Text())[0]
-		fmt.Printf("Your command was: %s\n", input)
-	}
+	startRepl()
 }
---
+++ repl.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	lower := strings.ToLower(text)
+	split := strings.Fields(lower)
+
+	return split 
+}
+
+func startRepl() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := cleanInput(scanner.Text())[0]
+		fmt.Printf("Your command was: %s\n", input)
+	}
+}
